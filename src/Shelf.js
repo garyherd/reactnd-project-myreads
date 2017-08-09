3128895dbaf3b775e85f0d9a73521d162f19e556
Gary Herd <garyherd5@gmail.com>
2017-08-08 23:47:44 -0500

    feat: Create Book and Shelf components
    These are initial set-ups to just start breaking up the big render
    in App.js. Event handling needs to be added to Book. Shelf is a
    functional component now, but it may need some local state.

---
+++ src/Shelf.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Book from './Book';
+import './App.css';
+
+const Shelf = (props) => {
+
+  const currentShelf = props.books.map((book) =>
+    <li key={book.id}>
+      <Book bookData={book} />
+    </li>
+  );
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {currentShelf}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Shelf;
+
+
